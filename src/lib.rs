1a2a44d7c1f9b3e49fa0e99186f5551ee01ccc44
slyo <sean.lyo@outlook.com>
2021-04-09 00:20:23 +0800

    find-min-in-rotated-sorted-arrary: Added.

--- src/lib.rs
+++ src/lib.rs
@@ -228,6 +228,79 @@ impl Solution {
     }
 }
 
+/// # 153. 寻找旋转排序数组中的最小值
+///
+/// 已知一个长度为 n 的数组, 预先按照升序排列, 经由 1 到 n 次 旋转 后, 得到输入数组。例如, 原数组 nums = [0,1,2,4,5,6,7] 在变化后可能得到：
+/// 若旋转 4 次, 则可以得到 [4,5,6,7,0,1,2]
+/// 若旋转 7 次, 则可以得到 [0,1,2,4,5,6,7]
+///
+/// 注意, 数组 [a[0], a[1], a[2], ..., a[n-1]] 旋转一次 的结果为数组 [a[n-1], a[0], a[1], a[2], ..., a[n-2]] 。
+///
+/// 给你一个元素值 互不相同 的数组 nums , 它原来是一个升序排列的数组, 并按上述情形进行了多次旋转。请你找出并返回数组中的 最小元素 。
+///  
+///
+/// 示例 1：
+///
+/// 输入：nums = [3,4,5,1,2]
+/// 输出：1
+/// 解释：原数组为 [1,2,3,4,5] , 旋转 3 次得到输入数组。
+///
+/// 示例 2：
+///
+/// 输入：nums = [4,5,6,7,0,1,2]
+/// 输出：0
+/// 解释：原数组为 [0,1,2,4,5,6,7] , 旋转 4 次得到输入数组。
+///
+/// 示例 3：
+///
+/// 输入：nums = [11,13,15,17]
+/// 输出：11
+/// 解释：原数组为 [11,13,15,17] , 旋转 4 次得到输入数组。
+///
+///  
+/// 提示：
+///     n == nums.length
+///     1 <= n <= 5000
+///     -5000 <= nums[i] <= 5000
+///     nums 中的所有整数 互不相同
+///     nums 原来是一个升序排序的数组, 并进行了 1 至 n 次旋转
+impl Solution {
+    /// # Test
+    ///
+    /// ```rust
+    /// use leetcode::Solution;
+    ///
+    /// assert_eq!(Solution::find_min_in_rotated_sorted_arrary(vec![4,5,6,7,0,1,2]), 0);
+    /// assert_eq!(Solution::find_min_in_rotated_sorted_arrary(vec![3,4,5,1,2]), 1);
+    /// assert_eq!(Solution::find_min_in_rotated_sorted_arrary(vec![11,13,15,17]), 11);
+    /// ```
+    pub fn find_min_in_rotated_sorted_arrary(nums: Vec<i32>) -> i32 {
+        // Commented for memory saving.
+        // assert!(nums.len() >= 1);
+        let mut low: usize = 0;
+        let mut high: usize = nums.len() - 1;
+        // if not rotated or len == 1
+        if nums[low] <= nums[high] {
+            return nums[low];
+        };
+        while low + 1 < high {
+            let pivot: usize = (low + high) / 2;
+            if nums[low] < nums[pivot] {
+                low = pivot;
+            } else {
+                high = pivot;
+            };
+            // Commented for memory saving.
+            // } else if nums[high] > nums[pivot] {
+            //     high = pivot;
+            // } else {
+            //     unreachable!();
+            // };
+        }
+        i32::min(nums[low], nums[high])
+    }
+}
+
 /// # Q300. Longest Increasing Subsequence
 ///
 /// 给你一个整数数组 nums ，找到其中最长严格递增子序列的长度。
